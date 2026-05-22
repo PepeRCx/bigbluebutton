e98709572c3ddece922d21381e63cb903069d145
pepe <josepp2508@gmail.com>
2026-05-22 00:45:42 -0600

    [FIX] timaiTTS: replace incorrect de-DE mapping with supported lv-LV locale

--- bbb-graphql-actions/src/services/timaiTTS.ts
+++ bbb-graphql-actions/src/services/timaiTTS.ts
@@ -16,7 +16,7 @@ export const SUPPORTED_TTS_LANGUAGES: Record<string, string> = {
   'en-US': 'en',
   'es-ES': 'es',
   'fr-FR': 'fr',
-  'de-DE': 'de',
+  'lv-LV': 'lv',
   'pt-BR': 'pt',
   'ru-RU': 'ru',
   'it-IT': 'it',
